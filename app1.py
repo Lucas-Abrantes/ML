058029fcee734f1c6d8dacf9fe5f887535d62d19
Lucas Abrantes Sarmento <lucas_abrantes25@hotmail.com>
2022-07-16 22:18:20 -0300

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraning = st.container()
+
+
+st.write('ola,mundo')
+with header:
+    st.title("Welcome! This is my first project of Data Science.")
+    st.text("In this project I look in to transactions of taxis yellows in NYC. ... ")
+
+with dataset:
+    st.header("NYC taxis dataset.")
+    st.text("I foudn this dataset ou ....")
+    taxi_data = pd.read_csv(r'C:\Users\Pichau\Downloads\cruso-streamlit\taxi_data.csv')
+    st.write(taxi_data.head())
+    st.write(taxi_data.describe())
+    pulocation_dist = pd.DataFrame(taxi_data['PULocationID'].value_counts())
+    st.bar_chart(pulocation_dist)
+
+with features:
+    st.header("The features I created.")
+
+with modelTraning:
+    st.header("Time to train the model")
+    st.text("Here you get to choose the hyperparameters of the model and see how the perfomance chenge.")
